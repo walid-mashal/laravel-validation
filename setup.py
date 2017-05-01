3513de8e24df7a51e32b7e54db632bad443f7f73
walid-netlinks <walid@neltinks.ws>
2017-05-01 09:59:10 +0430

    ordering directories

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'Python Validation',
+  packages = ['Python Validation'], 
+  version = '0.1',
+  description = 'laravel like data validation library for python language',
+  author = 'Walid Mashal',
+  author_email = 'walidmashal4@gmail.com',
+  url = 'https://github.com/walid-netlinks/Laravel-Validation.git', 
+  download_url = '',
+  keywords = ['validation', 'python', 'laravel'],
+  classifiers = [],
+)
