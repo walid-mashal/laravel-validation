f3027443d1697727bd7d56359931b015ff6b0fa5
Walid Mashal <walidmashal4@gmail.com>
2021-06-24 12:18:55 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 setup(
   name = 'Laravel-Validation',
   packages = ['Laravel-Validation'], 
-  version = '0.1',
+  version = '1.0',
   description = 'laravel like data validation library for python language',
   author = 'Walid Mashal',
   author_email = 'walidmashal4@gmail.com',
-  url = 'https://github.com/walid-netlinks/Laravel-Validation.git', 
+  url = 'https://github.com/walid-mashal/Laravel-Validation', 
   download_url = '',
   keywords = ['validation', 'python', 'laravel'],
   classifiers = [],
